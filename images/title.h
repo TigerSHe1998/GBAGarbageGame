9f947dbbca876b74ffedf5b5457162a10136d5b4
TigerSHe1998 <55221117+TigerSHe1998@users.noreply.github.com>
2020-11-14 20:45:28 +0800

    Add files via upload

---
+++ images/title.h
@@ -0,0 +1,23 @@
+/*
+ * Exported with nin10kit v1.7
+ * Invocation command was nin10kit --mode=3 title title.png 
+ * Time-stamp: Thursday 04/02/2020, 00:20:34
+ * 
+ * Image Information
+ * -----------------
+ * title.png 240@160
+ * 
+ * All bug reports / feature requests are to be filed here https://github.com/TricksterGuy/nin10kit/issues
+ */
+
+#ifndef TITLE_H
+#define TITLE_H
+
+extern const unsigned short title[38400];
+#define TITLE_SIZE 76800
+#define TITLE_LENGTH 38400
+#define TITLE_WIDTH 240
+#define TITLE_HEIGHT 160
+
+#endif
+
